554067887200db91f8f3f2f721cc0fd1d1bb94f8
Satria Pratama <satriapratamat7@gmail.com>
2021-10-22 14:06:05 +0700

    hari 4

---
+++ Hari2/Nomor5Hari2.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(palindrome("civic")) // true
+
+	fmt.Println(palindrome("katak")) // true
+
+	fmt.Println(palindrome("kasur rusak")) // true
+
+	fmt.Println(palindrome("mistar")) // false
+
+	fmt.Println(palindrome("lion")) // false
+
+}
