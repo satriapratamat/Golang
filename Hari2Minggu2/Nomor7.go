b9ea58022a2baf260fa37fb51ede190ffde0ddcf
Satria Pratama <satriapratamat7@gmail.com>
2021-10-27 12:08:26 +0700

    Hari2Minggu2

---
+++ Hari2Minggu2/Nomor7.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sumArr(arr []int) int {
+
+	var sum int
+	for _, v := range arr {
+		sum += v
+	}
+
+	return sum
+}
+
+func canOut(hand, deck []int) bool {
+	if len(hand) != len(deck) {
+		return false
+	}
+
+	for _, v := range hand {
+		for _, w := range deck {
+			if v == w {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func playingDomino(cards [][]int, deck []int) interface{} {
+	var closedCard []int
+
+	for i, v := range cards {
+		if canOut(v, deck) {
+			closedCard = append(closedCard, i)
+		}
+	}
+
+	if len(closedCard) == 0 {
+		return "tutup kartu"
+	}
+
+	outCard := cards[closedCard[0]]
+
+	for _, v := range closedCard {
+		if sumArr(cards[v]) > sumArr(outCard) {
+			outCard = cards[v]
+		}
+	}
+
+	return outCard
+}
+
+func main() {
+
+	fmt.Println(playingDomino([][]int{{6, 5}, {3, 4}, {2, 1}, {3, 3}}, []int{4, 3}))
+
+	// [3, 4]
+
+	fmt.Println(playingDomino([][]int{{6, 5}, {3, 3}, {3, 4}, {2, 1}}, []int{3, 6}))
+
+	// [6 5]
+
+	fmt.Println(playingDomino([][]int{{6, 6}, {2, 4}, {3, 6}}, []int{5, 1}))
+
+	// “tutup kartu”
+
+}
--- project-pertama/main.go
+++ project-pertama/main.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello world")
+	fmt.Println("Hello world")
 	var today = "Tuesday"
 
 	switch today {
-		case "Tuesday":
-			fmt.Println("Cie baru selasa")
-		case "Monday":
-			fmt.Println("Baru juga senin")
-		case default:
-			fmt.Println("Semangat bro bentar lagi")
+	case "Tuesday":
+		fmt.Println("Cie baru selasa")
+	case "Monday":
+		fmt.Println("Baru juga senin")
+	default:
+		fmt.Println("Semangat bro bentar lagi")
 	}
 }
