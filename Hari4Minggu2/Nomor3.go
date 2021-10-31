14a054a720272a81a607c53a347a942c3b03a806
Satria Pratama <satriapratamat7@gmail.com>
2021-10-31 14:03:21 +0700

    Hari4Minggu2 fix

--- Hari4Minggu2/Nomor3.go
+++ Hari4Minggu2/Nomor3.go
@@ -9,18 +9,23 @@ var res = make(map[int]int)
 
 func jump(a, b int) int {
 	frogJump := math.Abs((float64(a - b)))
+	fmt.Println("frogJump line12 adalah = ", frogJump)
 	return int(frogJump)
 }
 
 func Frog(jumps []int) int {
 
 	nJump := len(jumps) - 1
+	fmt.Println("nJump line19 adalah = ", nJump)
 	res[0] = 0
 	res[1] = jump(jumps[0], jumps[1])
 	for i := 2; i <= nJump; i++ {
 		min2 := float64(res[i-2] + jump(jumps[i], jumps[i-2]))
+		// fmt.Println("min2 adalah = ", min2)
 		min1 := float64(res[i-1] + jump(jumps[i], jumps[i-1]))
+		// fmt.Println("min1 adalah = ", min1)
 		res[i] = int(math.Min(min2, min1))
+		fmt.Println("res i adalah = ", res[i])
 	}
 
 	return res[nJump]
